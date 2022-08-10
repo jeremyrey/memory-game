33ff346ed2daef7cba481e8ce5ca7fde850a7753
id <jeremy.rey@inthememory.com>
2022-08-10 13:04:51 +0200

    fix homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from "../theme";
-
+/* eslint-disable react/prop-types */
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
